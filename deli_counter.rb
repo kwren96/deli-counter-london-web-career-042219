0016fbb290b4f3e504d9b1437900ca79b7dd1918
Katherine Wren <kwren@outlook.com>
2019-04-19 08:23:16 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -6,13 +6,13 @@ length = katz_deli.count
   if length == 0
     puts 'The line is currently empty.'
   else 
-    katz_deli.each_with_index do |customer, index|
     deliline = []
+    katz_deli.each_with_index do |customer, index|
     index += 1
     deliline << "#{index}. #{customer}"
+    end
     list = deliline.join(', ')
     puts "The line is currently: #{list}"
-    end
 end
 end
 
