ceab5d814b47dacb9d61d0b12efb226c61d0d5d3
Katherine Wren <kwren@outlook.com>
2019-04-19 08:11:10 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,23 @@
 # Write your code here.
+katz_deli = []
+
+def line(katz_deli)
+length = katz_deli.count
+  if length == 0
+    puts 'The line is currently empty.'
+  else katz_deli.each_with_index do |customer, index|
+  deliline = []
+  index += 1
+  
+    puts "The line is currently: #{index}. #{customer}"
+  end
+end
+end
+
+def take_a_number(katz_deli, string)
+  
+end
+
+def now_serving
+  
+end
